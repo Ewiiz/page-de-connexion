ff1a27c58f27933594e08e3098df458422f1b1c8
Ewizz <bverpet46@gmail.com>
2023-06-10 00:21:52 +0400

    API CRUD

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+const routerUser = require('./routes/user.routes')
+
+app.use(express.json())
+
+// MIDDLEWARE
+
+const logger = require('./middlewares/infosRequetes')
+const bodyParser = require('./middlewares/parseurDonnes')
+app.use(bodyParser.json())
+app.use(logger)
+app.use(cors())
+
+require('dotenv').config()
+
+const corsOptions = {
+    origin: ['http://192.168.1.81:3000', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}
+
+app.use(cors(corsOptions));
+
+mongoose
+    .connect(process.env.MONGO_CONNEXION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connexion à la BDD réussie.')
+    })
+    .catch((err) => {
+        console.log(`Erreur: ${err.message}`)
+    })
+
+app.use('/api', routerUser)
+
+module.exports = app
